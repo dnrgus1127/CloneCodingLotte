6a5d0ae181b07f218d6c5cbc60622dbf02aa744f
dnrgus1127 <dnrgus1127@naver.com>
2022-09-09 01:54:42 +0900

    FEAT(Main) : Background video, img ADD

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import { GlobalStyle } from './style/global';
 import { ThemeProvider } from 'styled-components'
 import { lightTheme } from './style/theme';
+import { Main } from './components/Main';
 
 const Layout = styled.div`
 
@@ -14,9 +15,12 @@ function App() {
     <ThemeProvider theme={lightTheme}>
     
     <GlobalStyle/>
-    <Layout>
+    <Layout className='Layout'>
       <Header/>
+      <Main/>
+
     </Layout>
+
     </ThemeProvider>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,140 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Swiper, SwiperSlide } from "swiper/react";
+import '../css/Main.css'
+import 'swiper/scss'
+import 'swiper/scss/navigation'
+import 'swiper/scss/pagination'
+import "swiper/css/effect-fade";
+import BGImg1 from '../assets/images/main-img-business.gif'
+import BGImg2 from '../assets/images/main-img-reqruit.gif'
+import BGImg3 from '../assets/images/main-img-investment.gif'
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade } from 'swiper';
+import BGVideo1 from '../assets/images/video/Main_ESG.mp4'
+import BGVideo2 from '../assets/images/video/Main_Factory.mp4'
+
+
 
 const Container = styled.div`
+    display: block;
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+
+`
+
+const MainBanner = styled.div`
+    position: relative;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    
+`
+
+const StyledSwiper = styled(Swiper)`
+    background-color: white;
+    height: 100%;
+    /* width: 300px; */
+    .mainContentItem {
+        position: relative;
+        overflow: hidden;
+        height: 100vh;
+
+    }
 
 
+`
+const Bg = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    left: 0;
+    top:0;
+    video {
+        position: absolute;
+        z-index: inherit;
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+    .cover {
+        position: absolute;
+        top:0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,.3);
+    }
+`
+
+const Img = styled.img`
+    /* width: 100%; */
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+    
 `
 
 
 
+
 export const Main = () => {
-  return (
-    <Container>
-        
-    </Container>
-  )
+    const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+    return (
+        <Container style={{ height: vh }}>
+            <MainBanner>
+                <StyledSwiper
+                    modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, EffectFade]}
+                    spaceBetween={50}
+
+                    slidesPerView={1}
+                    scrollbar={{ draggable: true }}
+                    navigation
+                    // pagination={{ clickable: true }}
+                    autoplay={{ delay: 2000 }}
+                    loop={true}
+                    effect='fade'
+                    speed={1000}
+
+
+                >
+                    <SwiperSlide>
+                        <Bg >
+                            <video autoplay="autoplay" loop="loop" muted="muted" playsinline>
+                                <source src={BGVideo2} type="video/mp4"></source>
+                            </video>
+                            <div className='cover'>
+                            </div>
+                        </Bg>
+                    </SwiperSlide>
+                    <SwiperSlide >
+                        <Bg >
+                            <video autoplay="autoplay" loop="loop" muted="muted" playsinline>
+                                <source src={BGVideo1} type="video/mp4"></source>
+                            </video>
+                            <div className='cover'>
+                            </div>
+                        </Bg>
+                    </SwiperSlide>
+
+                    <SwiperSlide>
+                        <Bg>
+                            <Img src={BGImg1} alt=''></Img>
+                        </Bg>Bg
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Bg >
+                            <Img src={BGImg2} alt=''></Img>
+                        </Bg>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <Bg >
+                            <Img src={BGImg3} alt=''></Img>
+                        </Bg>
+                    </SwiperSlide>
+                </StyledSwiper>
+
+            </MainBanner>
+        </Container>
+    )
 }
